e1b90e68d31ac419c821f49be78d6ad74d8cca21
Kyriakos Schwarz <kyriakos.schwarz@uzh.ch>
2021-08-02 18:26:29 +0200

    synergy improvements

--- deepadr/model_gnn_ogb.py
+++ deepadr/model_gnn_ogb.py
@@ -15,8 +15,8 @@ from torch_scatter import scatter_mean
 
 class GNN(torch.nn.Module):
 
-    def __init__(self, num_tasks, num_layer = 5, emb_dim = 300, 
-                    gnn_type = 'gin', virtual_node = True, residual = False, drop_ratio = 0.5, JK = "last", graph_pooling = "mean", with_edge_attr=False, return_multilayer=False):
+    def __init__(self, num_layer = 5, emb_dim = 300, 
+                    gnn_type = 'gat', virtual_node = True, residual = False, drop_ratio = 0.5, JK = "last", graph_pooling = "mean", with_edge_attr=False):
         '''
             num_tasks (int): number of labels to be predicted
             virtual_node (bool): whether to add virtual node or not
@@ -28,10 +28,9 @@ class GNN(torch.nn.Module):
         self.drop_ratio = drop_ratio
         self.JK = JK
         self.emb_dim = emb_dim
-        self.num_tasks = num_tasks
+#         self.num_tasks = num_tasks
         self.graph_pooling = graph_pooling
         self.with_edge_attr = with_edge_attr
-        self.return_multilayer = return_multilayer
 
         if self.num_layer < 2:
             raise ValueError("Number of GNN layers must be greater than 1.")
@@ -40,7 +39,7 @@ class GNN(torch.nn.Module):
         if virtual_node:
             self.gnn_node = GNN_node_Virtualnode(num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type)
         else:
-            self.gnn_node = GNN_node(num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type, with_edge_attr=self.with_edge_attr, return_multilayer=self.return_multilayer)
+            self.gnn_node = GNN_node(num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type, with_edge_attr=self.with_edge_attr)
 
 
         ### Pooling function to generate whole-graph embeddings
@@ -52,15 +51,15 @@ class GNN(torch.nn.Module):
             self.pool = global_max_pool
         elif self.graph_pooling == "attention":
             self.pool = GlobalAttention(gate_nn = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, 1)))
-        elif self.graph_pooling == "set2set":
-            self.pool = Set2Set(emb_dim, processing_steps = 2)
+#         elif self.graph_pooling == "set2set":
+#             self.pool = Set2Set(emb_dim, processing_steps = 2)
         else:
             raise ValueError("Invalid graph pooling type.")
 
-        if graph_pooling == "set2set":
-            self.graph_pred_linear = torch.nn.Linear(2*self.emb_dim, self.num_tasks)
-        else:
-            self.graph_pred_linear = torch.nn.Linear(self.emb_dim, self.num_tasks)
+#         if graph_pooling == "set2set":
+#             self.graph_pred_linear = torch.nn.Linear(2*self.emb_dim, self.num_tasks)
+#         else:
+#             self.graph_pred_linear = torch.nn.Linear(self.emb_dim, self.num_tasks)
 
     def forward(self, x, edge_index, edge_attr, batch):
                 #x, edge_index, edge_attr, batch = batched_data.x, batched_data.edge_index, batched_data.edge_attr, batched_data.batch
@@ -72,7 +71,7 @@ class GNN(torch.nn.Module):
             
 #         print("h_node_1 shape:", h_node[1].shape)
 
-        if self.return_multilayer:
+        if self.JK == "multilayer":
             h_graphs = [self.pool(h, batch) for h in h_node]
 #             print("h_graph_1 shape:", h_graphs[1].shape)
             
@@ -99,7 +98,7 @@ def _init_model_params(named_parameters):
 
 class DeepAdr_SiameseTrf(nn.Module):
 
-    def __init__(self, input_dim, dist, expression_dim, num_classes=2, do_softmax=True):
+    def __init__(self, input_dim, dist, expression_dim, num_classes=2, drop=0.5, do_softmax=True):
         
         super().__init__()
         
@@ -115,7 +114,9 @@ class DeepAdr_SiameseTrf(nn.Module):
             self.dist = nn.CosineSimilarity(dim=1)
             self.alpha = 1
 
-        self.Wy = nn.Linear(2*input_dim+1+expression_dim, num_classes)
+        self.Wy = nn.Linear(2*input_dim+1+expression_dim, expression_dim)
+        self.Wy2 = nn.Linear(expression_dim, num_classes)
+        self.drop = nn.Dropout(drop)
         # perform log softmax on the feature dimension
         self.log_softmax = nn.LogSoftmax(dim=-1)
 
@@ -139,6 +140,9 @@ class DeepAdr_SiameseTrf(nn.Module):
         
         out = torch.cat([Z_a, Z_b, dist, Z_e], axis=-1)
         y = self.Wy(out)
+        y = self.drop(y)
+        y = self.Wy2(y)
+
         
         if self.do_softmax:
             return self.log_softmax(y), dist
@@ -146,7 +150,7 @@ class DeepAdr_SiameseTrf(nn.Module):
             return y, dist
 
 class ExpressionNN(nn.Module):
-    def __init__(self, D_in=8785, H1=400, H2=300, D_out=100, drop=0.5):
+    def __init__(self, D_in=8785, H1=800, H2=300, D_out=64, drop=0.5):
         super(ExpressionNN, self).__init__()
         # an affine operation: y = Wx + b
         self.fc1 = nn.Linear(D_in, H1) # Fully Connected
